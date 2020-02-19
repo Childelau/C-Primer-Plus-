df8a3f428138cd2c7527122fa7f438873c5be1e2
livermore-xieqiao <1299337429@qq.com>
2020-02-19 15:50:23 +0800

    test1

---
+++ Chapter2/myfirst/cmake-build-debug/CMakeFiles/myfirst.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfirst.dir/main.cpp.obj"
+  "myfirst.exe"
+  "myfirst.exe.manifest"
+  "myfirst.lib"
+  "myfirst.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myfirst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
